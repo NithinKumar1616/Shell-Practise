897896cccbcc0e1adfaea85ef30d894e1e3597a9
Nithin Kumar Tarigoppala <nithinkumar1616@gmail.com>
2025-01-01 12:06:38 +0530

    shell practise

---
+++ Area_of_a_Circle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Write a script to calculate the area of a circle. The user will provide the radius.
+Formula: Area = π * r^2 (Use 3.14 for π).
+
+echo "What is the radius of the circle?"
+read a
+
+if ( $a -eq 0 ); then
+echo "Radius of the circle should be more than 0"
+exit 1
+
+Area=$((3.14 * ($a*$a)))
+
+echo "Area of the circle is: $Area"
