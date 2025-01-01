fb1aa6611659bc92113aea72952e6fb4e564e421
Nithin Kumar Tarigoppala <nithinkumar1616@gmail.com>
2025-01-01 11:57:25 +0530

    shell practise

--- Square_of_a_Number.sh
+++ Square_of_a_Number.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #Write a script to calculate the square of a number provided by the user.
+
+echo "Enter the number:"
+read a
+
+square=$((a*a))
+
+echo "square of a number that you have provided is: $sqaure"
