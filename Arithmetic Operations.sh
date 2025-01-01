d103ef1203181d53d17d488771d015b5d7ecbd71
Nithin Kumar Tarigoppala <nithinkumar1616@gmail.com>
2025-01-01 06:49:57 +0530

    shell practise

---
+++ Arithmetic Operations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Write a script to take two numbers as input and perform addition, subtraction, multiplication, and division on them. Display the results.
+
+echo "Enter the value of a:"
+read a
+
+echo "Enter the value of b:"
+read b
+
+Sum(($a+$b))
+
+subtraction(($a-$b))
+
+Multiplication(($a*$b))
+
+Division(($a/$b))
+
+echo "Sum of a&b is $Sum"
+
+echo "Substraction of a&b is $Substraction"
+
+echo "Multiplication of a&b is $Multiplication"
+
+echo "Division of a&b is $Division"
--- Sum_of_Two_Numbers.sh
+++ Sum_of_Two_Numbers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Write a script to take two numbers as input from the user and display their sum.
 
+#read helps us to prompt the user to enter the value and then it will assign that value to the variable
 echo "Enter the value for a::"
 read a
 echo "Enter the value for b::"
