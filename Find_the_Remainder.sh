5b8e320cf2b103b4e993e834569e088055936be7
Nithin Kumar Tarigoppala <nithinkumar1616@gmail.com>
2025-01-01 11:54:24 +0530

    shell practise

--- Find_the_Remainder.sh
+++ Find_the_Remainder.sh
@@ -4,10 +4,9 @@
 a=$1
 b=$2
 
-Div=$((a/b))
-
 if [ $b -eq 0 ]; then 
 echo "Division is not possible"
 exit 1
 fi
+reminder=$(($a%$b))
 echo "The reminder of a/b is $Div"
---
+++ Square_of_a_Number.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#Write a script to calculate the square of a number provided by the user.
